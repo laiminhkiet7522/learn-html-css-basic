3a7d2cdd096a8884d11f0ce9d30d67a6188d8b43
Minh Kiet <kietminh070502@gmail.com>
2023-12-04 16:08:47 +0700

    Section 9 - Implementing a Sticky Navigation Bar

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -7,20 +7,20 @@ yearEl.textContent = currentYear;
 const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
 
-btnNavEl.addEventListener('click',function(e) {
+btnNavEl.addEventListener('click', function (e) {
   e.preventDefault();
   headerEl.classList.toggle("nav-open");
 });
 
 //Smooth scrolling animation
 const allLinks = document.querySelectorAll("a:link");
-allLinks.forEach(function(link) {
-  link.addEventListener('click',function(e) {
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
     e.preventDefault();
     const href = link.getAttribute('href');
 
     //Scroll back to top
-    if(href === "#"){
+    if (href === "#") {
       window.scrollTo({
         top: 0,
         behavior: "smooth"
@@ -28,7 +28,7 @@ allLinks.forEach(function(link) {
     }
 
     //Scroll to other links
-    if(href !== "#" && href.startsWith("#")){
+    if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({
         behavior: "smooth"
@@ -36,11 +36,31 @@ allLinks.forEach(function(link) {
     }
 
     //Close mobile navigation
-    if(link.classList.contains("main-nav-link")){
+    if (link.classList.contains("main-nav-link")) {
       headerEl.classList.toggle("nav-open");
     }
   });
 });
+
+//Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  if(ent.isIntersecting === false){
+    console.log(ent);
+    document.body.classList.add("sticky");
+  }
+  if (ent.isIntersecting === true) {
+    console.log(ent);
+    document.body.classList.remove("sticky");
+  }
+}, {
+  //In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+});
+obs.observe(sectionHeroEl);
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
